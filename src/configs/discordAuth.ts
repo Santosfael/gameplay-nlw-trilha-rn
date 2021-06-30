c7f557bc312aae481ff0df39e78165e3acf10fb4
Rafael Rocha <rafaelsantoscv_@hotmail.com>
2021-06-30 11:11:32 -0300

    Continuation of last commit

---
+++ src/configs/discordAuth.ts
@@ -0,0 +1,13 @@
+const REDIRECT_URI = 'redirect_uri';
+const SCOPE = 'scope';
+const RESPONSE_TYPE = 'token';
+const CLIENT_ID = 'client_id';
+const CDN_IMAGE = 'https://cdn.discordapp.com';
+
+export {
+    REDIRECT_URI,
+    SCOPE,
+    RESPONSE_TYPE,
+    CLIENT_ID,
+    CDN_IMAGE
+}
